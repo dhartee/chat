0e08ccf7863b6868da023fc52d111cecc84e6360
DharTee <121927489+dhartee@users.noreply.github.com>
2025-10-24 19:12:59 +0530

    config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+                    apiKey: "AIzaSyBayS0D9xJZNstCf1D2Wk3aYt2Xll4RoY4",
+                    authDomain: "law-dhartee.firebaseapp.com",
+                    projectId: "law-dhartee",
+                    storageBucket: "law-dhartee.firebasestorage.app",
+                    messagingSenderId: "667742651569",
+                    appId: "1:667742651569:web:72eea345d97974acc60e90",
+                    measurementId: "G-EC6BD85GSX"
+                };
